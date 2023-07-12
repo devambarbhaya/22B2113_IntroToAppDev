63c14fa8df576267388fec9db9d14eefbc39801b
devambarbhaya <22b2113@iitb.ac.in>
2023-07-12 21:43:37 +0530

    Merge remote changes

---
+++ first/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Devam\\Downloads\\flutter_windows_3.10.5-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Devam\\Music\\APP_DEV\\first" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Devam\\Downloads\\flutter_windows_3.10.5-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Devam\\Music\\APP_DEV\\first"
+  "FLUTTER_ROOT=C:\\Users\\Devam\\Downloads\\flutter_windows_3.10.5-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Devam\\Music\\APP_DEV\\first\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Devam\\Music\\APP_DEV\\first"
+  "FLUTTER_TARGET=C:\\Users\\Devam\\Music\\APP_DEV\\first\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC80NWY2ZTAwOTExMGRmNGYzNGVjMmNmOTlmNjNjZjczYjcxYjdhNDIwLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Devam\\Music\\APP_DEV\\first\\.dart_tool\\package_config.json"
+)
